ee9bcd6530ec3d36d3cd5689c5e8c4e7d0c37e77
Marcoseven <marco.caliciotti3@gmail.com>
2021-11-15 13:25:55 +0100

    Aggiunte modifiche file vue (SiteHeader)
    Inserite due contenitori in template e aggiunte delle proprietà in style.

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,9 +1,24 @@
 <template>
-	<div></div>
+	<div class="container">
+		<div class="contentHeader w-75">
+			<img src="../assets/img/spotify-logo.svg" alt="logo Spotify" />
+		</div>
+	</div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style lang="scss">
+@import "../assets/scss/variables.scss";
+
+.contentHeader {
+	background-color: $header_mainCard_backgroundColor;
+	margin: 0 auto;
+	img {
+		width: 4%;
+		margin: 0.5rem 1rem;
+	}
+}
+</style>
