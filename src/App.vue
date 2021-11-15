e521bfe990b0972def2d08df5646c01b49d5aa49
Marcoseven <marco.caliciotti3@gmail.com>
2021-11-15 12:24:06 +0100

    Creata struttura di base dell'esercizio
    Creata struttura facendo uso di Vue CLI, ed inseriti nella cartella components due file vue, SiteHeader e SiteMain, mentre nella cartella assets inserita la cartella img, con all'interno l'immagine del logo e la cartella scss, che continere due file scss. Ed infine importato Bootstrap.

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="app">
+		<SiteHeader />
+		<SiteMain />
+	</div>
+</template>
+
+<script>
+import SiteHeader from "./components/SiteHeader";
+import SiteMain from "./components/SiteMain";
+
+export default {
+	name: "App",
+	components: {
+		SiteHeader,
+		SiteMain,
+	},
+};
+</script>
+
+<style lang="scss">
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
+@import "./assets/scss/common.scss";
+@import "./assets/scss/variables.scss";
+</style>
