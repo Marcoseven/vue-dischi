bb7df56bad3d59215155ba822d11abe7daad4168
Marcoseven <marco.caliciotti3@gmail.com>
2021-11-16 17:02:57 +0100

    Creato file vue (SearchBox)
    Inserita struttura di base di Vue e aggiunto altro per far in modo che funzionasse un selettore che filtra gli album per genere.

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="SearchBox">
+		<label for="genre">Scegli il genere: </label>
+		<select
+			name="genre"
+			id="genre"
+			v-model="searchGenre"
+			@change="$emit('search-genre', searchGenre)"
+		>
+			<option value="All">All</option>
+			<option value="Rock">Rock</option>
+			<option value="Pop">Pop</option>
+			<option value="Jazz">Jazz</option>
+			<option value="Metal">Metal</option>
+		</select>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			searchGenre: "",
+		};
+	},
+};
+</script>
+
+<style lang="scss">
+.SearchBox {
+	padding: 0.5rem;
+	label {
+		color: #fff;
+		padding: 0 0.5rem;
+	}
+}
+</style>
